10a65863b5609eaf12f47fb3a317c37d399ae255
Sana Mohiuddin <smohi002@ucr.edu>
2019-10-07 16:48:37 -0700

    Use if-else statements to pass tests

--- array.sh
+++ array.sh
@@ -5,5 +5,13 @@ INPUTS=("first input" "second input" "third input")
 for input in "${INPUTS[@]}"
 do
     echo "./c-echo ${input}"
-    ./c-echo ${input}
+    output=$(./c-echo ${input})
+    echo "Output: \"${output}\""
+    echo "Input: \"${input}\""
+    if [ "${output}" = "${input}" ]
+    then
+        echo "Test passed"
+    else
+        echo "Test failed"
+    fi
 done
--- c-echo.cpp
+++ c-echo.cpp
@@ -3,7 +3,11 @@
 int main(int argv, char** argc) {
     // Skip the first argc index because its the program
     for(int i = 1; i < argv; i++) {
-       std::cout << argc[i] << " ";
+       std::cout << argc[i];
+        // Print a whitespace after all but the last iteration
+       if(i < argv - 1) {
+          std::cout << " ";
+       }
     }
 std::cout << std::endl;
 }
