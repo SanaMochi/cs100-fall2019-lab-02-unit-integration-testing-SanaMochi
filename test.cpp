dc7a6498449287d9e69e26c54850bacd530f94ec
Sana Mohiuddin <smohi002@ucr.edu>
2019-10-07 17:56:57 -0700

    added 3 more test cases

--- test.cpp
+++ test.cpp
@@ -11,3 +11,24 @@ int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
+
+TEST(EchoTest, EmptyString) {
+    char* test_val[1]; test_val[0] = "./c-echo";
+    EXPECT_EQ("", echo(1,test_val));
+}
+
+TEST(EchoTest, LongString) {
+  char* test_val[6]; test_val[0] = "./c-echo"; test_val[1] = "This"; test_val[2] = "is"; test_val[3] = "a"; test_val[4] = "long"; test_val[5] = "sentence";
+
+  EXPECT_EQ("This is a long sentence", echo(6, test_val);
+}
+
+TEST(EchoTest, OneValue) {
+    char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "Single";
+    EXPECT_EQ("Single", echo(2,test_val));
+}
+
+TEST(EchoTest, Number) {
+    char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "10";
+    EXPECT_EQ("10", echo(2,test_val));
+}
